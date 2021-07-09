f551adde4ca8a06dd7fcbf3a3ae42b5df161df9b
imranilyas <68575807+imranilyas@users.noreply.github.com>
2021-07-09 06:43:26 -0700

    Small Comments on Code

--- scouter/src/Components/Anime/AnimeLeft.tsx
+++ scouter/src/Components/Anime/AnimeLeft.tsx
@@ -3,6 +3,7 @@ import { Row } from "reactstrap";
 import AnimeCard from "./AnimeCard/AnimeCard";
 import './Anime.css';
 const AnimePageLeft:React.FC = (props) => {
+    //returns anime card which has all the information on a specific anime
     return(
         <div>
         <Row id='lRow'></Row>
