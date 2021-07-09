34b9e0cc9546ecdc783ef3efb1ccd4fc1abd5f66
imranilyas <68575807+imranilyas@users.noreply.github.com>
2021-07-09 09:57:30 -0700

    7.38 coverage

--- scouter/src/Components/Users/UserProfileCard/UserProfileCard.test.tsx
+++ scouter/src/Components/Users/UserProfileCard/UserProfileCard.test.tsx
@@ -1,11 +1,11 @@
 
-import { shallow } from 'enzyme';
+import enzyme from '../../../setupTests';
 import React from 'react'
 import UserProfileCard from './UserProfileCard';
 
 describe('<UserProfileCard />', () => {
     it('returns profile card', () => {
-        const wrapper = shallow(<UserProfileCard/>);
+        const wrapper = enzyme.shallow(<UserProfileCard/>);
         expect(wrapper.find("div")).toHaveLength(1);
     })
 })
